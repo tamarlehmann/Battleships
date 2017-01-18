72f66fe74a2332f1fbb73f4d808ce1344c6ffc60
Sergey Drozdov <srgdzv@gmail.com>
2017-01-18 15:34:08 +0000

    Health Points

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.feature 'Personalised battle', :type => :feature do
+feature 'Personalised battle', :type => :feature do
   scenario 'Typed names appear on the screen' do
     visit '/'
 
@@ -9,5 +9,17 @@ RSpec.feature 'Personalised battle', :type => :feature do
     click_button 'submit'
 
     expect(page).to have_text 'Tamar vs. Serjey'
+  end
+end
+
+feature 'si the first player winning?', :type => :feature do
+  scenario 'Displays the 2nd player\'s health bar' do
+    visit '/'
+
+    fill_in :name1, :with => 'Tamar'
+    fill_in :name2, :with => 'Serjey'
+    click_button 'submit'
+
+    expect(page).to have_content 'Serjey: 100HP'
   end
 end
