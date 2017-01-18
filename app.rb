eadde684ef52f0599f75d070cc93c7a498636721
Sergey Drozdov <srgdzv@gmail.com>
2017-01-18 16:39:30 +0000

    Attackgit add .! option

--- app.rb
+++ app.rb
@@ -22,5 +22,11 @@ class Battleship < Sinatra::Base
     erb(:play)
   end
 
+  get '/attack' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+    erb(:attack)
+end
+
   run! if app_file == $0
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,9 +7,17 @@ feature 'Personalised battle', :type => :feature do
   end
 end
 
-feature 'so the first player winning?', :type => :feature do
-  scenario 'Displays the 2nd player\'s health bar' do
+feature 'is the first player winning?', :type => :feature do
+  scenario 'Displays the 2nd player\'s health points' do
     sign_in_and_play
     expect(page).to have_content 'Serjey: 100HP'
+  end
+end
+
+feature 'Winning the battle' do
+  scenario 'Attacking' do
+    sign_in_and_play
+    click_button 'Attack!!!'
+    expect(page).to have_content 'Tamar attacked Serjey'
   end
 end
